bb2d39eeb708f81ed4b7f7a62c42fc8ce132a19d
yakisoba-ya <143864653+yakisoba-ya@users.noreply.github.com>
2024-02-06 13:47:35 +0900

    Add files via upload

---
+++ kawachiwabachien/js/script.js
@@ -0,0 +1,45 @@
+
+$('.hero__view__box').slick({
+  fade:true,//切り替えをフェードで行う。初期値はfalse。
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
+  speed:1500,//スライドの動きのスピード。初期値は300。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  slidesToShow: 1,//スライドを画面に3枚見せる
+  slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+  arrows: true,//左右の矢印あり
+  prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+  dots: true,//下部ドットナビゲーションの表示
+      pauseOnFocus: false,//フォーカスで一時停止を無効
+      pauseOnHover: false,//マウスホバーで一時停止を無効
+      pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+});
+
+$('.main__content-04__scroll').slick({
+  arrows: false,//左右の矢印はなし
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
+  speed: 6900,//スライドのスピード。初期値は300。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  pauseOnHover: false,//オンマウスでスライドを一時停止させるかどうか。初期値はtrue。
+  pauseOnFocus: false,//フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
+  cssEase: 'linear',//動き方。初期値はeaseですが、スムースな動きで見せたいのでlinear
+  slidesToShow: 4,//スライドを画面に4枚見せる
+  slidesToScroll: 1,//1回のスライドで動かす要素数
+  responsive: [
+    {
+    breakpoint: 769,//モニターの横幅が769px以下の見せ方
+    settings: {
+      slidesToShow: 2,//スライドを画面に2枚見せる
+    }
+  },
+  {
+    breakpoint: 426,//モニターの横幅が426px以下の見せ方
+    settings: {
+      slidesToShow: 1.5,//スライドを画面に1.5枚見せる
+    }
+  }
+]
+
+});
